d953123fc9cda21722e98725a7ae83f102cbc516
YOGGA HADI SUTISNA <117752398+hadiyogga@users.noreply.github.com>
2022-12-31 17:02:42 +0700

    Yogga Hadi
    Tugas UKM

---
+++ kelompok2/admin/module/informasi/index.php
@@ -0,0 +1,34 @@
+ <!--sidebar end-->
+      
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+
+              <div class="row">
+                  <div class="col-lg-12 main-chart">
+						<h3>Pembuat Aplikasi</h3>
+					
+				<br>	Nama UKM : DANISWARA JAYA</br>
+<br>Topik Penelitian : Sistem Informasi Penjualan Berbasis Web</br>
+
+ 
+<br>Hasil Rancangan kelompok 2</br>
+<br>Nama kelompok :</br>
+<br>1.Obaja Krisna Siburian</br>
+<br>2.Yogga Hadi Sutisna</br>
+<br>3.Azka salsabila</br>
+<br>4.Eky Septi Virginia</br>
+
+								</tr>
+								</form>
+							</tbody>
+						</table>
+						<div class="clearfix" style="padding-top:41%;"></div>
+				  </div>
+              </div>
+          </section>
+      </section>
+	
